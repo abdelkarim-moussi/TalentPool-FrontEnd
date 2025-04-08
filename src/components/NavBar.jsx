954c59b509b40c65f8b375ace6c19afa289808de
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-04-08 11:40:10 +0000

    create a primaryButton components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import PrimaryButton from "./PrimaryButton";
 
 function NavBar(){
 
@@ -6,8 +7,7 @@ function NavBar(){
         <a href="" className="text-white">TalentPool</a>
         <ul className="flex items-center text-white">
             <li className="mr-4 text-sm capitalize hover:"><a href="">JobAds</a></li>
-            <li className="mr-4 text-sm capitalize hover:"><a href="">login</a></li>
-            <li className="mr text-sm capitalize hover:"><a href="">sign up</a></li>
+            <PrimaryButton text='log in' />
         </ul>
     </nav>
     )
---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,10 @@
+export default function PrimaryButton({ text }) {
+  return (
+    <a
+      href=""
+      className="px-4 py-0.5 bg-[#D9E0A4] rounded-md text-black hover:bg-[#ABB17B] hover:text-white"
+    >
+      {text}
+    </a>
+  );
+}
