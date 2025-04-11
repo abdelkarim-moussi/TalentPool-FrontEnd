1138e19b708384aee11f6f447db9be891da2f59c
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-04-11 14:42:50 +0000

    login form : input validation, regex , reactPasswordChecklist

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 
-const {
-  register,
-  formState: { errors },
-} = useFormContext();
-
-const inputError = findInputError(errors, label);
-const isInvalid = isFormInvalid(inputError);
-
 const Input = ({
   label,
   id,
@@ -24,15 +16,6 @@ const Input = ({
         {label}
       </label>
 
-      <AnimatePresence mode="wait" initial={false}>
-        {isInvalid && (
-          <InputError
-            message={inputError.error.message}
-            key={inputError.error.message}
-          />
-        )}
-      </AnimatePresence>
-
       <input
         className={`w-full border border-black h-[35px] rounded-lg ${extra}`}
         id={id}
@@ -40,34 +23,9 @@ const Input = ({
         value={value}
         onChange={onChange}
         type={type}
-        {...register(label, {
-          required: {
-            value: true,
-            message: "required",
-          },
-        })}
       />
     </div>
   );
-};
-
-const InputError = ({ message }) => {
-  return (
-    <motion.p
-      className="flex items-center gap-1 px-2 font-semibold text-red-500 bg-red-100 rounded-md"
-      {...framer_error}
-    >
-      <MdError />
-      {message}
-    </motion.p>
-  );
-};
-
-const framer_error = {
-  initial: { opacity: 0, y: 10 },
-  animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: 10 },
-  transition: { duration: 0.2 },
 };
 
 export default Input;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,12 +3,32 @@ import Button from "./Button";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Input from "./Input";
+import ReactPasswordChecklist from "react-password-checklist";
+import { rules } from "eslint-plugin-react-refresh";
+import { isValid } from "date-fns";
 
 const LoginForm = () => {
-  const methods = useForm();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const validateEmail = (value) => {
+    const regex = /^\S+@\S+\.\S+$/;
+    return regex.test(value);
+  };
+
+  const validatePassword = (value) => {};
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+
+    if (!validateEmail(value)) {
+      setError("Invalid email format");
+    } else {
+      setError("");
+    }
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -29,7 +49,7 @@ const LoginForm = () => {
   return (
     <div className="flex flex-col justify-center items-center w-full max-w-[400px] mt-5">
       <h3 className="text-md font-semibold uppercase">Log in</h3>
-      <FormProvider {...methods}>
+
       <form onSubmit={handleLogin} className="w-full flex flex-col gap-4">
         <Input
           label="Email"
@@ -38,8 +58,9 @@ const LoginForm = () => {
           type="email"
           value={email}
           div_extra="flex-col"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleChange}
         />
+        {error && <p className="text-sm text-red-500 mt-[-10px]">{error}</p>}
 
         <Input
           label="Password"
@@ -49,6 +70,12 @@ const LoginForm = () => {
           value={password}
           div_extra="flex-col"
           onChange={(e) => setPassword(e.target.value)}
+        />
+        <ReactPasswordChecklist className="text-sm" iconSize={10}
+          rules={["minLength", "specialChar", "capital", "letter"]}
+          minLength = {8}
+          value={password}
+          onChange={(isValid)=>{}}
         />
 
         <Button type="submit" text="login" />
@@ -60,7 +87,6 @@ const LoginForm = () => {
           </Link>
         </p>
       </form>
-      </FormProvider>
     </div>
   );
 };
